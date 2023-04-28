98803ac058dfaffe28dd99eb11feb5505faaef6d
Daniel Bernardo <everaldo.daniel@academico.ifpb.edu.br>
2023-04-27 21:10:22 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,39 +1,28 @@
+#Importação
 from socket import *
-
+#Cliente
 host = gethostname()
 port = 55416
-
 client = socket(AF_INET, SOCK_STREAM)
 client.connect((host,port))
-msg2 = client.recv(1024)
-print(msg2.decode())
+#Recebe o Menu
+menu = client.recv(1024)
+print(menu.decode())
+#Loop de conexão
 while 1:
-    msg = input('Qual cotação deseja saber? ')
-    client.send(msg.encode())
-    if msg == '0':
-        print()
-        print('Até a próxima :)')
+    #Cliente faz escolha da cotação
+    msg = int(input('Qual cotação deseja saber? '))
+    #Envia a escolha para o servidor
+    client.send(str(msg).encode())
+    #Classe de moedas
+    moedas = {1:'Dólar', 2:'Euro', 3:'Bitcoin'}
+    #Encerra a conexão
+    if msg == 0:
+        print('\nAté a próxima :)')
         client.close
         break
-    elif msg == '1':
-        dolar = client.recv(1024).decode()
-        print()
-        print(f'A cotação atual do Dólar é R${dolar}')
-        print()
-    elif msg == '2':
-        euro = client.recv(1024).decode()
-        print()
-        print(f'A cotação atual do Euro é R${euro}')
-        print()
-    elif msg == '3':
-        btc = client.recv(1024).decode()
-        print()
-        print(f'A cotação atual do Bitcoin é R${btc}')
-        print()
+    #Recebe a cotação escolhida
+    elif (msg) > 0 and (msg) < 4:
+        print(f'\nA cotação atual do {moedas[msg]} é R${client.recv(1024).decode()}\n')
     else:
-        invalido = client.recv(1024).decode()
-        print()
         print('Opção inválida. Tente novamente.')
-        print()
-    
-    
